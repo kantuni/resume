058e2e3164c991f0114000615bd2318fb2de667a
Alex Kantuni <akantuni@gmail.com>
2020-11-27 01:45:29 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,129 @@
+import streamlit as st
+
+st.title("Hovhannes Kantuni")
+st.image("https://avatars.githubusercontent.com/u/63314087?s=200")
+st.markdown("Email: [hkantuni@gmail.com](mailto:hkantuni@gmail.com)")
+st.markdown("GitHub: [hkantuni](https://github.com/hkantuni)")
+
+st.header("Summary")
+"""
+- Junior web developer with more than a year of experience in the design and development of various applications.
+- Excellent team player with a strong ability to network and with a drive for tangible results.
+- High commitment to quality, reliability, and a self-motivating workstyle. Excellent interpersonal skills and a professional approach.
+- Willingness and ability to learn new subjects quickly.
+- Ability to develop Graphical User Interfaces, data structures, and algorithms.
+"""
+
+"Throughout my professional career, I have gained experience in project management, engineering, energy, IT, procurement, budgeting, monitoring, and logistics (see the link to my old resume below). As a result of my extensive experience in IT and engineering, I became interested in software development. Due to the COVID-19 pandemic, I finally had enough free time to become proficient in full-stack web development."
+"[Link to my old resume](https://github.com/hkantuni/Resume/blob/main/HK%20old_resume.md)"
+
+st.header("Technical Skills")
+st.subheader("Front-end web development")
+"""
+- HTML5
+- CSS3
+- Bootstrap
+- Material UI
+- JavaScript (ES6)
+- React
+- jQuery
+"""
+
+st.subheader("Back-end web development")
+"""
+- Node
+- Express.js
+- EJS
+- REST APIs
+- Passport.js
+- Authentication with JWTs
+- Mongoose
+"""
+
+st.subheader("Databases")
+"""
+- MongoDB
+"""
+
+st.subheader("Platforms / Services")
+"""
+- Firebase
+- Heroku
+- Github Pages
+- MongoDB Atlas
+"""
+
+st.subheader("Other")
+"""
+- Git
+- Markdown
+"""
+
+st.header("Projects")
+
+st.subheader("Problem solving on Kattis")
+"""
+- More than 100 problems solved.
+- GitHub: https://github.com/hkantuni/Kattis
+- Platform: https://open.kattis.com/countries/ARM
+"""
+
+st.subheader("Time Management System")
+"""
+- Languages: JavaScript (ES6, Node)
+- Libraries:
+    - Client: React, React Router, React Toast Notifications, Bootstrap
+    - API: Express, Mongoose, JWT
+- Deployment: https://hk-tms.web.app
+    - Client: Firebase
+    - API: Heroku
+- GitHub: https://github.com/hkantuni/TMProject
+"""
+
+st.subheader("Personal Website")
+"""
+- Languages: ES6
+- Libraries: React, React Router, React Pagination, React Image Zoom
+- Deployment: Firebase
+- Web: https://hk-diecast-models.web.app/
+"""
+
+st.header("Education")
+
+"""
+- Master of Economics
+Economics and Financial Management
+St. Petersburg University of Foreign Economic Relations
+2006-2008
+"""
+
+"""
+- Master of Engineering
+Radio and Electronic Engineering
+Yerevan State Engineering University
+1990-1991
+"""
+
+"""
+- Bachelor of Engineering
+Radio and Electronic Engineering
+Yerevan State Engineering University
+1986-1990
+"""
+
+
+st.header("Language Skills")
+
+"""
+- English
+- Armenian (native)
+- Russian (native)
+- German
+"""
+
+st.header("Certifications")
+
+"""
+- [Udemy] The Complete 2020 Web Development Bootcamp
+- [Microsoft] Microsoft Certified Professional
+"""
